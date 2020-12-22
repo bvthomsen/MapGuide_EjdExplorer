8a3184d166eb87f44522d459d0a381a421ece2f3
Bo Victor Thomsen <bvt@aestas.dk>
2020-12-22 21:58:03 +0100

    ny web request med fast token

--- lifa/App_Code/LifaService.cs
+++ lifa/App_Code/LifaService.cs
@@ -65,5 +65,44 @@ public class LifaService : System.Web.Services.WebService {
         
         return returnValue;
     }
+
+    [WebMethod]
+    public string GetLIFAticket_exp(string KMSuser, string KMSpwd, string wkt)
+    {
+        
+        WebClient wclient = new WebClient();
+
+        //string KMSuri = "https://api.dataforsyningen.dk/service?request=GetTicket&login={0}&password={1}";
+        //string ticket = wclient.DownloadString(string.Format(KMSuri, KMSuser, KMSpwd));
+        string ticket =    "7686d17ccf142c489825874ebf362440";
+
+        string url = "http://www.kortviser.dk/UsersPublic/Handlers/LIFAExternalIntegrationServiceREST.ashx?callback=";
+        string function = "ConvertToCadastralDistrictsAndRealProperties";
+
+        string dataEksempel = string.Format("{{\"function\":\"{0}\",\"wkt\":\"{1}\",\"ticket\":\"{2}\"}}",function,wkt,ticket);
+        dataEksempel = this.Server.UrlEncode(dataEksempel);
+
+        System.Net.HttpWebRequest myReq = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+
+        byte[] dataEksempelByteArray = Encoding.UTF8.GetBytes(dataEksempel);
+
+        myReq.Method = "POST";
+        myReq.ContentType = "application/x-www-form-urlencoded";
+        myReq.ContentLength = dataEksempelByteArray.Length;
+
+        using (Stream dataStream = myReq.GetRequestStream())
+        {
+            dataStream.Write(dataEksempelByteArray, 0, dataEksempelByteArray.Length);
+        }
+        System.Net.HttpWebResponse resp = (System.Net.HttpWebResponse)myReq.GetResponse();
+        System.IO.Stream responseStream = resp.GetResponseStream();
+        System.IO.StreamReader rd = new System.IO.StreamReader(responseStream);
+
+        string returnValue = rd.ReadToEnd();
+
+        returnValue = Regex.Replace(returnValue, "[^0-9]", "");
+        
+        return returnValue;
+    }
     
 }
